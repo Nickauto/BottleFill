e220cc7cb07e3bff7006b68ab424bd753cd46252
Nicholas Collins <nicholas.collins@autoliv.com>
2022-12-16 12:29:51 -0700

    refac: created bottle_t

--- app/main.f90
+++ app/main.f90
@@ -1,7 +1,15 @@
 program main
   use BottleFill, only: Fill
+  use Bottle_m, only: Bottle_t
   implicit none
 
+  type(Bottle_t) :: bottle
+  type(Bottle_t) :: bottle2
+  
+  bottle = Bottle_t(1.0)
+  bottle2 = Bottle_t(1.3435)
+  print *, bottle%to_string()
+  print *, bottle2%to_string()
   call Fill()
   !Test
 end program main
---
+++ src/Bottle_m.f90
@@ -0,0 +1,50 @@
+module bottle_m
+    implicit none
+    private
+
+    public Bottle_t
+
+    type Bottle_t
+        private
+        real :: volume
+        real :: radius
+        real :: m = 0
+        real :: E = 0
+        real :: P_b = 0
+        real :: T_b = 0
+        real :: rho_b = 0
+        real :: c
+        real :: T_static
+    contains
+        procedure, public :: to_string
+    end type
+
+    interface Bottle_t
+        module procedure constructor
+    end interface
+
+
+contains
+    pure function constructor(volume) result(Bottle)
+        real, intent(in) :: volume
+        type(Bottle_t) :: Bottle
+        real :: radius_
+
+        radius_ = ((3.0 / (4.0*3.1415)) * volume) ** (1.0/3.0)
+        Bottle%volume = volume 
+        Bottle%radius = radius_
+    end function
+
+    pure function to_string(self) result(string)
+        class(Bottle_t), intent(in) :: self
+        character(len=150) :: string
+
+        
+
+        write(string,1) new_line("C"), self%volume
+        1 format("Bottle_t: ", A1,&
+            "   Volume = ", f10.2)
+    end function
+
+
+end module
